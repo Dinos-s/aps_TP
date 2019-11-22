d085619e0758febafbb40be6ab9e41c304f360b9
Gabriel Ramos <gabriel02ramos@gmail.com>
2019-11-22 17:01:53 -0300

    Final

--- ufa/src/ex3.java
+++ ufa/src/ex3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * ex3
  */
@@ -5,17 +7,33 @@ public class ex3 {
 
     public static void main(String[] args) {
         double x = 1, elev = 2, fat = 2;
-        double divi, y, z, t;
+        double divi, ax, ax2, y;
         Scanner h = new Scanner(System.in);
         
-       System.out.println("Digite o dividendo: ")
-        t = h.nextDouble();
-        t = Math.pow(t, 2);
-        divi = t;
+       System.out.println("Digite o dividendo: ");
+        y = h.nextDouble();
+        y = Math.pow(y, 2);
+        divi = y;
         for (int i = 0; i <= 100; i++){
-            z = 0;
-            y = fat;
-            
+            ax2 = 0;
+            ax = fat;
+            while (fat > 1) {
+                ax = ax * (fat - 1);
+                if (fat > 1) {
+                     ax2++;
+                }
+                fat --;
+            }
+            if (i % 2 == 0) {
+                x -= divi / ax;
+            }else{
+                x += divi / ax;
+            }
+            System.out.println(divi + "/"+ax+" = " + x);
+            fat += 2 + ax2;
+            elev += elev + 2;
+            divi = divi * y;
         }
+        h.close();
     }
   }
--- ufa/src/ex4.java
+++ ufa/src/ex4.java
@@ -8,38 +8,87 @@ public class ex4 {
     public static void main(String[] args) {
         Scanner r = new Scanner(System.in);
         float media = 0, B, C, O;
-        int op = 0;
+        int op = 0, x =0;
 
-        System.out.println("-----MENU-----");
-        System.out.println("Escolha o sexo das especies: ");
-        System.out.println("1- masculino;");
-        System.out.println("2- feminino;");
+        System.out.println("----------------MENU---------------------");
+        System.out.println("Escolha o sexo das especies a seguir: ");
+        System.out.println("1- bolvinos machos;");
+        System.out.println("2- bolvinos fêmeas;");
+        System.out.println("3- caprinos machos;");
+        System.out.println("4- caprinos fêmeas;");
+        System.out.println("5- ovinos machos;");
+        System.out.println("6- ovinos fêmeas:");
         op = r.nextInt();
 
         switch (op) {
             case 1:
-                System.out.println("Indique o peso bolvino: ");
+                System.out.println("Quantidade de animais: ");
+                x = r.nextInt();
+
+                System.out.println("Indique o peso total dos bolvinos total: ");
                 B = r.nextFloat();
-                System.out.println("Indique Peso cabrino: ");
+
+                media = B / x;
+                System.out.println("Média dos Bolvinos machos: "+media);
+
+                break;
+
+            case 3:  
+                 System.out.println("Quantidade de animais: ");
+                x = r.nextInt();  
+
+                System.out.println("Indique o peso total dos cabrino total: ");
                 C = r.nextFloat();
-                System.out.println("Indique Peso ovinos: ");
+
+                media = C / x;
+                System.out.println("Média dos Caprinos machos: "+media);
+
+                break;
+
+            case 5:
+                System.out.println("Quantidade de animais: ");
+                x = r.nextInt();    
+
+                System.out.println("Indique o Peso total ovinos total: ");
                 O = r.nextFloat();
 
-                media = O + B + C/3;
-                System.out.println("A media de pesos entre os animais é: " + media);
+                media = O / x;
+                System.out.println("Média dos Ovinos machos: " + media);
 
                    break;
 
-                case 2:
-                System.out.println("Indique o peso bolvino: ");
+            case 2:
+                System.out.println("Quantidade de animais: ");
+                x = r.nextInt();
+
+                System.out.println("Indique o peso total dos bolvinos fêmeas total: ");
                 B = r.nextFloat();
-                System.out.println("Indique Peso cabrino: ");
+
+                media = B / x;
+                System.out.println("Média dos Bolvinos fêmeas: "+media);
+
+                break;
+            case 4:
+                System.out.println("Quantidade de animais: ");
+                x = r.nextInt();
+
+                System.out.println("Indique o peso total dos cabrinos fêmeas: ");
                 C = r.nextFloat();
-                System.out.println("Indique Peso ovinos: ");
+
+                media = C / x;
+                System.out.println("Média dos Cabrinos fêmeas: "+media);
+
+                break;
+
+            case 6:   
+                System.out.println("Quantidade de animais: ");
+                x = r.nextInt();
+
+                System.out.println("Indique o peso total dos ovinos fêmeas: ");
                 O = r.nextFloat();
 
-                media = O + B + C/3;
-                System.out.println("A media de pesos entre os animais é: " + media);
+                media = O /x;
+                System.out.println("Média dos Ovinos fêmeas: " + media);
                     break;
 
             default:
