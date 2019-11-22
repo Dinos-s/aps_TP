75a1d7f6937d47f49b8e9dfd6512bdc487f7b594
Dinos-s <56043956+Dinos-s@users.noreply.github.com>
2019-11-22 08:32:37 -0300

    Update ex3.java

--- ufa/src/ex3.java
+++ ufa/src/ex3.java
@@ -5,7 +5,17 @@ public class ex3 {
 
     public static void main(String[] args) {
         double x = 1, elev = 2, fat = 2;
-        double divi, y, z t;
-       
+        double divi, y, z, t;
+        Scanner h = new Scanner(System.in);
+        
+       System.out.println("Digite o dividendo: ")
+        t = h.nextDouble();
+        t = Math.pow(t, 2);
+        divi = t;
+        for (int i = 0; i <= 100; i++){
+            z = 0;
+            y = fat;
+            
+        }
     }
   }
